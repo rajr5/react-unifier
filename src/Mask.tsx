d01ebaf88f035880cb3f8161d06341c2db4b2bd7
Suchit <rajrikhy2@gmail.com>
2020-08-16 23:20:42 -0500

    Add mask stories

--- src/Mask.tsx
+++ src/Mask.tsx
@@ -1,22 +1,53 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import cx from "classnames";
 import * as React from "react";
-import {MaskProps} from "./UnifiedCommon";
-import {Mask as GestaltMask} from "./gestalt";
+import {fromClassName, identity, Style, toProps} from "./gestalt/style";
+import {bind, range} from "./gestalt/transforms";
+import {MaskProps, Rounding} from "./UnifiedCommon";
 
-type Rounding = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | "circle";
+const styles = require("./gestalt/Mask.module.css");
+const borders = require("./gestalt/Borders.module.css");
 
-export class Mask extends React.Component<MaskProps, {}> {
-  render() {
-    const shape = this.props.shape;
-    let rounding: Rounding = 0;
-    if (shape === "rounded") {
-      rounding = 4;
-    } else if (shape === "circle") {
-      rounding = "circle";
-    }
-    return (
-      <GestaltMask {...this.props} rounding={rounding}>
-        {this.props.children}
-      </GestaltMask>
-    );
+require("./gestalt/Typography.module.css");
+
+const getRoundingStyle = (r: Rounding): Style => {
+  if (typeof r === "number") {
+    return bind(range("rounding"), borders)(r);
   }
+
+  if (r === "circle") {
+    return fromClassName(borders.circle);
+  }
+
+  return identity();
+};
+
+export function Mask(props: MaskProps) {
+  let {
+    children,
+    rounding = 0,
+    width,
+    height,
+    maxWidth,
+    maxHeight,
+    willChangeTransform = true,
+    wash = false,
+    shape,
+  } = props;
+  if (shape === "rounded") {
+    rounding = 4;
+  } else if (shape === "circle") {
+    rounding = "circle";
+  }
+  return (
+    <div
+      className={cx(styles.Mask, toProps(getRoundingStyle(rounding)).className, {
+        [styles.willChangeTransform]: willChangeTransform,
+      })}
+      style={{width, height, maxWidth, maxHeight}}
+    >
+      {children}
+      {wash && <div className={styles.wash} />}
+    </div>
+  );
 }
--- src/UnifiedCommon.ts
+++ src/UnifiedCommon.ts
@@ -804,12 +804,15 @@ export interface SpinnerProps {
 }
 
 export interface MaskProps {
+  children?: any;
   shape?: "circle" | "rounded" | "square";
   height?: number | string;
   width?: number | string;
   maxHeight?: number | string;
   maxWidth?: number | string;
   rounding?: "circle" | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+  willChangeTransform?: boolean;
+  wash?: boolean;
 }
 
 export interface IconRowProps {
@@ -825,6 +828,8 @@ export interface LinkProps {
   onClick?: () => void;
   target?: null | "blank";
 }
+
+export type Rounding = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | "circle";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface HeadingProps {
